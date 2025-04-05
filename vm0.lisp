6ae8827aa66eb9652a272e3aceef6bc9902e4c37
Mattias Brändström <svala@spotify.com>
2025-04-05 12:03:51 +0200

    Working stack based VM

--- vm0.lisp
+++ vm0.lisp
@@ -3,58 +3,134 @@
 
 (in-package :vm0)
 
-(defparameter *stack* `())
 
-(defun vm0-push (i)
-  (push i *stack*))
+(defun execute (instruction stack sp)
+  (labels ((vm0-push (value) (setf (aref stack sp) value) (incf sp))
+           (vm0-pop () (aref stack (decf sp))))
+    (let ((control-directive '(:continue)))
+      (case (car instruction)
+        (:push (vm0-push (cadr instruction)))
+        (:pop (vm0-pop))
+        (:dup (let ((a (vm0-pop)))
+                (vm0-push a) (vm0-push a)))
+        (:over (let ((a (vm0-pop)) (b (vm0-pop)))
+                 (vm0-push b) (vm0-push a) (vm0-push b)))
+        (:swap (let ((a (vm0-pop)) (b (vm0-pop)))
+                 (vm0-push a) (vm0-push b)))
+        (:rot (let ((a (vm0-pop)) (b (vm0-pop)) (c (vm0-pop)))
+                (vm0-push b) (vm0-push a) (vm0-push c)))
+        (:add (vm0-push (+ (vm0-pop) (vm0-pop))))
+        (:sub (let ((b (vm0-pop)) (a (vm0-pop)))
+                (vm0-push (- a b))))
+        (:inc (let ((a (vm0-pop)))
+                (vm0-push (1+ a))))
+        (:dec (let ((a (vm0-pop)))
+                (vm0-push (1- a))))
+        (:mul (vm0-push (* (vm0-pop) (vm0-pop))))
+        (:div (let ((b (vm0-pop)) (a (vm0-pop)))
+                (if (zerop b)
+                    (setf control-directive '(:trap :divide-by-zero))
+                    (vm0-push (truncate a b)))))
+        (:mod (let ((b (vm0-pop)) (a (vm0-pop)))
+                (if (zerop b)
+                    (setf control-directive '(:trap :divide-by-zero))
+                    (vm0-push (nth-value 1 (truncate a b))))))
+        (:eq (vm0-push (if (= (vm0-pop) (vm0-pop)) 1 0)))
+        (:lt (let ((b (vm0-pop)) (a (vm0-pop)))
+               (vm0-push (if (< a b) 1 0))))
+        (:gt (let ((b (vm0-pop)) (a (vm0-pop)))
+               (vm0-push (if (> a b) 1 0))))
+        (:jmp (setf control-directive `(:jump ,(cadr instruction))))
+        (:jz (when (zerop (vm0-pop))
+               (setf control-directive `(:jump ,(cadr instruction)))))
+        (:jnz (when (not (zerop (vm0-pop)))
+                (setf control-directive `(:jump ,(cadr instruction)))))
+        (:print (format t "~A~%" (vm0-pop)))
+        (:halt (setf control-directive '(:done)))
+        (otherwise (setf control-directive `(:trap :unknown-instruction ,(car instruction)))))
+      (values control-directive sp))))
 
-(defun vm0-pop ()
-  (pop *stack*))
-
-
-(defun execute (instruction)
-  (case (car instruction)
-    (:push (vm0-push (cadr instruction)) '(:continue))
-    (:pop (vm0-pop) '(:continue))
-    (:dup (let ((a (vm0-pop))) (vm0-push a) (vm0-push a)) '(:continue))
-    (:add (vm0-push (+ (vm0-pop) (vm0-pop))) '(:continue))
-    (:sub (let ((b (vm0-pop)) (a (vm0-pop))) (vm0-push (- a b))) '(:continue))
-    (:mul (vm0-push (* (vm0-pop) (vm0-pop))) '(:continue))
-    ;; div
-    ;; mod
-    (:eq (vm0-push (if (= (vm0-pop) (vm0-pop)) 1 0)) '(:continue))
-    (:lt (let ((b (vm0-pop)) (a (vm0-pop))) (vm0-push (if (< a b) 1 0))) '(:continue))
-    (:gt (let ((b (vm0-pop)) (a (vm0-pop))) (vm0-push (if (> a b) 1 0))) '(:continue))
-    (:jmp `(:jump ,(cadr instruction)))
-    (:jz (if (zerop (vm0-pop)) (cons :jump (cadr instruction)) '(:continue)))
-    (:jnz (if (not (zerop (vm0-pop))) (cons :jump (cadr instruction)) '(:continue)))
-    (:print (format t "~A~%" (vm0-pop)) '(:continue))
-    (:halt '(:done))
-    (otherwise (error "unknown instruction: ~S" (car instruction)))))
-
-(defun run (program)
-  (let ((pc 0))
+(defun run (program &optional (stack (make-array 1024 :initial-element 0)))
+  (let ((pc 0) (sp 0))
     (loop while (< pc (length program)) do
-      (let ((control-directive (execute (elt program pc))))
+      (multiple-value-bind (control-directive new-sp) (execute (aref program pc) stack sp)
         (case (car control-directive)
           (:continue (incf pc))
           (:jump (setf pc (cadr control-directive)))
+          (:trap
+           (format t "trap: ~S~%" (cdr control-directive))
+           (return))
           (:done (return))
-          (otherwise (error "unknown control directive: ~S" (car control-directive))))))))
+          (otherwise (error "unknown control directive: ~S" (car control-directive))))
+        (setf sp new-sp)))))
 
-(run #(
-       (:push 40)
-       (:push 2)
-       (:add)
-       (:push 20)
-       (:sub)
-       (:print)
-       (:jmp 9)
-       (:push 100)
-       (:print)
-       (:push 200)
-       (:print)
-       (:halt)
-       (:push 1337)
-       (:print)
-       ))
+(defun assemble (program)
+  (let ((program (if (vectorp program) program (coerce program 'vector)))
+        (the-labels (make-hash-table)))
+    ;; resolve label addresses
+    (loop with addr = 0
+          for instruction across program
+          do (case (car instruction)
+               (:label (setf (gethash (cadr instruction) the-labels) addr))
+               (otherwise (incf addr))))
+    ;; resolve jump labels
+    (coerce
+     (loop for instruction across program
+           for x = (case (car instruction)
+                     (:label nil)
+                     ((:jmp :jz :jnz) (let ((target (gethash (cadr instruction) the-labels)))
+                                        (unless target
+                                          (error "undefined label: ~S" (cadr instruction)))
+                                        (list (car instruction) target)))
+                     (otherwise instruction))
+           when x
+             collect x)
+     'vector)))
+
+(defun syntax (program)
+  (let ((program (if (vectorp program) program (coerce program 'vector))))
+    (loop for instruction across program
+          do (unless
+                 (case (car instruction)
+                   (:push (typep (cadr instruction) 'integer))
+                   ((:jmp :jz :jnz :label) (typep (cadr instruction) 'keyword))
+                   (otherwise (typep (car instruction) 'keyword)))
+               (error "syntax error: ~S" instruction)))
+    program))
+
+;;; factorial
+(run
+ (assemble
+     (syntax
+      '((:push 1)                      ; product
+        (:push 5)                      ; counter, factorial to compute
+
+        (:label :loop)
+        (:dup)                          ; copy counter for check
+        (:push 1)
+        (:eq)
+        (:jnz :done)
+
+        (:dup)                          ; copy counter
+        (:rot)                          ; now: counter product counter
+        (:mul)                          ; counter * product
+        (:swap)                         ; product result under counter
+        (:dec)                          ; dec counter
+        (:jmp :loop)
+
+        (:label :done)
+        (:pop)                          ; discard counter
+        (:print)
+        (:halt)))))
+
+(run
+ (assemble
+     (syntax
+      '(
+        (:push 1)
+        (:push 2)
+        (:push 3)
+        (:print)
+        (:print)
+        (:print)
+        ))))
