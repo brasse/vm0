354b00b2123b4f667f0fa6e73e9bba33211d2689
Mattias Brändström <thebrasse@brasse.org>
2025-04-06 20:58:06 +0200

    Add assemble-and-run that runs the whole pipeline
    And a max(a, b) demo program.

--- vm0.lisp
+++ vm0.lisp
@@ -153,94 +153,108 @@
                (error "syntax error: ~S" instruction)))
     program))
 
+(defun assemble-and-run (&key program trace)
+  (run (assemble (macro (syntax program)))
+       :trace trace))
+
 ;;; factorial
-(run
- (assemble
-     (macro
-      (syntax
-       '((:push 1)                     ; product
-         (:push 5)                     ; counter, factorial to compute
+(assemble-and-run
+ :program '((:push 1)                      ; product
+            (:push 5)                      ; counter, factorial to compute
 
-         (:label :loop)
-         (:dup)                         ; copy counter for check
-         (:push 1)
-         (:eq)
-         (:jnz :done)
+            (:label :loop)
+            (:dup)                         ; copy counter for check
+            (:push 1)
+            (:eq)
+            (:jnz :done)
 
-         (:dup)                         ; copy counter
-         (:rot)                         ; now: counter product counter
-         (:mul)                         ; counter * product
-         (:swap)                        ; product result under counter
-         (:dec)                         ; dec counter
-         (:jmp :loop)
+            (:dup)                         ; copy counter
+            (:rot)                         ; now: counter product counter
+            (:mul)                         ; counter * product
+            (:swap)                        ; product result under counter
+            (:dec)                         ; dec counter
+            (:jmp :loop)
 
-         (:label :done)
-         (:pop)                         ; discard counter
-         (:print)
-         (:halt))))))
+            (:label :done)
+            (:pop)                         ; discard counter
+            (:print)
+            (:halt)))
 
 ;;; fibonacci
-(run
- (assemble
-     (macro
-      (syntax
-       '((:push 0)                      ; a fib(n - 2)
-         (:push 1)                      ; b fib(n - 1)
-         (:push 10)                     ; n
+(assemble-and-run
+ :program '((:push 0)                            ; a fib(n - 2)
+            (:push 1)                            ; b fib(n - 1)
+            (:push 10)                           ; n
 
-         (:label :loop)
-         (:dup)                         ; n
-         (:push 0)
-         (:eq)
-         (:jnz :done)
+            (:label :loop)
+            (:dup)                               ; n
+            (:push 0)
+            (:eq)
+            (:jnz :done)
 
-         ;; next = a + b
-         (:push 2) (:pick)              ; a
-         (:push 2) (:pick)              ; b
-         (:add)                         ; next
+            ;; next = a + b
+            (:push 2) (:pick)                    ; a
+            (:push 2) (:pick)                    ; b
+            (:add)                               ; next
 
-         ;; update a and b
-         (:push 3) (:roll)              ; b → a
-         (:push 2) (:roll)              ; next → b
+            ;; update a and b
+            (:push 3) (:roll)                    ; b → a
+            (:push 2) (:roll)                    ; next → b
 
-         (:dec)                         ; n = n - 1
+            (:dec)                               ; n = n - 1
 
-         ;; clean up old a
-         (:swap)                        ; bring a to TOS
-         (:pop)                         ; discard it
+            ;; clean up old a
+            (:swap)                              ; bring a to TOS
+            (:pop)                               ; discard it
 
-         (:jmp :loop)
+            (:jmp :loop)
 
-         (:label :done)
-         (:pop)                         ; discard n
-         (:print)                       ; print b
-         (:halt))))))
+            (:label :done)
+            (:pop)                               ; discard n
+            (:print)                             ; print b
+            (:halt)))
 
 ;;; sum [0 .. n]
-(run
- (assemble
-     (macro
-      (syntax
-       '((:push 0)                      ; sum = 0
-         (:push 5)                      ; n = 5
+(assemble-and-run
+ :program '((:push 0)                            ; sum = 0
+            (:push 5)                            ; n = 5
 
-         (:label :loop)
-         (:dup)                         ; copy n
-         (:push 0)
-         (:eq)
-         (:jnz :done)
+            (:label :loop)
+            (:dup)                               ; copy n
+            (:push 0)
+            (:eq)
+            (:jnz :done)
 
-         (:over)                        ; copy sum
-         (:over)                        ; copy n
-         (:add)                         ; sum + n
-         (:rot)                         ; n result sum
-         (:pop)                         ; drop old sum
-         (:swap)                        ; result n
-         (:dec)                         ; n = n - 1
+            (:over)                              ; copy sum
+            (:over)                              ; copy n
+            (:add)                               ; sum + n
+            (:rot)                               ; n result sum
+            (:pop)                               ; drop old sum
+            (:swap)                              ; result n
+            (:dec)                               ; n = n - 1
 
-         (:jmp :loop)
+            (:jmp :loop)
 
-         (:label :done)
-         (:pop)                         ; remove counter
-         (:print)                       ; print sum
-         (:halt))))))
+            (:label :done)
+            (:pop)                               ; remove counter
+            (:print)                             ; print sum
+            (:halt)))
+
+;;; max(a, b)
+(assemble-and-run
+ :program '((:push 72)                   ; a
+            (:push 42)                   ; b
+
+            (:over)
+            (:over)
+            (:gt)
+            (:jnz :a-is-bigger)
+
+            (:swap)
+
+            (:label :a-is-bigger)
+            (:pop)
+
+            (:label :done)
+            (:print)
+            (:halt)))
