f9ca6363f60562c2e338debe74b77a36382a4ca8
Mattias Brändström <svala@spotify.com>
2025-04-28 21:49:46 +0200

    Add some examples written in Stak
    Choose to name the language Stak since it's running on a stack machine.

---
+++ examples/examples-stak.lisp
@@ -0,0 +1,51 @@
+(in-package :vm0)
+
+(defparameter +factorial-stak-tr+
+  '((fn fact-0 (n acc)
+     (if (= n 1)
+         acc
+         (fact-0 (- n 1) (* acc n))))
+    (fn fact (n) (fact-0 n 1))
+    (print (fact 1000))))
+
+(defparameter +fibonacci-stak+
+  '((fn fib (n)
+     (if (<= n 2)
+         1
+         (+ (fib (- n 1)) (fib (- n 2)))))
+    (print (fib 15))))
+
+(defparameter +fibonacci-stak-tr+
+  '((fn fib-0 (n a b)
+     (if (= n 0)
+         a
+         (fib-0 (- n 1) b (+ a b))))
+    (fn fib (n) (fib-0 n 0 1))
+    (print (fib 500))))
+
+(defparameter +even-odd-stak-tr+
+  '((fn even (n)
+     (if (= n 0)
+         0
+         (odd (- n 1))))
+    (fn odd (n)
+     (if (= n 0)
+         1
+         (even (- n 1))))
+    (print (even 100001))))
+
+(defparameter +count-down-stak-tr+
+  '((fn count-down (n)
+     (if (= n 0)
+         0
+         (count-down (- n 1))))
+    (print (count-down 100000))))
+
+(defparameter +counter-stak-tr+
+  '((fn counter (current limit)
+     (if (> current limit)
+         current
+         (progn
+           (print current)
+           (counter (+ current 1) limit))))
+    (print (counter 0 20))))
